9a1a1049e97379464e42c5e2f8979f91fa7a880c
FabianMonjaraz <d.fabian.monjaraz@gmail.com>
2019-10-09 14:14:28 -0500

    Configuration for the wifi antenna

--- Arduino/test_softwareserial/test_softwareserial.ino
+++ Arduino/test_softwareserial/test_softwareserial.ino
@@ -1,56 +1,54 @@
-/*
-  Software serial multple serial test
-
- Receives from the hardware serial, sends to software serial.
- Receives from software serial, sends to hardware serial.
-
- The circuit:
- * RX is digital pin 2 (connect to TX of other device)
- * TX is digital pin 3 (connect to RX of other device)
-
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
- Not all pins on the Leonardo support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
- based on Mikal Hart's example
-
- This example code is in the public domain.
-
- */
 #include <SoftwareSerial.h>
+#include <string.h>
 
-SoftwareSerial mySerial(2, 3); // RX, TX
+SoftwareSerial mySerial(2, 3);  //RX,TX
 
-void setup()
-{
-  // Open serial communications and wait for port to open:
-  Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Native USB only
-  }
+//  Arduino pin 2 (RX) to ESP8266 TX
+//  Arduino pin 3 to voltage divider then to ESP8266 RX
+//  Connect GND from the Arduiono to GND on the ESP8266
+//  Pull ESP8266 CH_PD HIGH
 
+// When a command is entered in to the serial monitor on the computer 
+// the Arduino will relay it to the ESP8266
 
-  Serial.println("Goodnight moon!");
+ 
+int LEDPIN = 13;
 
-  // set the data rate for the SoftwareSerial port
-  mySerial.begin(38400);
-  mySerial.println("Hello, world?");
+void sendCommand(char *cmd){
+    mySerial.println(cmd);
+    while ( mySerial.available() )   {  Serial.write( mySerial.read() );  }
+    delay(250);
 }
-
-void loop() // run over and over
-{
-  if (mySerial.available())
-    Serial.write(mySerial.read());
-  if (Serial.available())
-    mySerial.write(Serial.read());
+ 
+void initConnection(){
+    sendCommand("AT+RST");
+    sendCommand("ATE0");
+    sendCommand("AT+GMR");
+    sendCommand("AT+CWJAP_CUR=\"Bastion\",\"#4rt0r14s#\"");
+    delay(1000);
 }
-
-
+void setup() 
+{
+    pinMode(LEDPIN, OUTPUT);
+ 
+    Serial.begin(9600);     // communication with the host computer
+    //while (!Serial)   { ; }
+ 
+    // Start the software serial for communication with the ESP8266
+    mySerial.begin(9600);  
+ 
+    Serial.println("");
+    Serial.println("Remember to to set Both NL & CR in the serial monitor.");
+    Serial.println("Ready");
+    Serial.println("");    
+    initConnection();
+}
+ 
+void loop() 
+{
+    // listen for communication from the ESP8266 and then write it to the serial monitor
+    if ( mySerial.available() )   {  Serial.write( mySerial.read() );  }
+ 
+    // listen for user input and send it to the ESP8266
+    if ( Serial.available() )       {  mySerial.write( Serial.read() );  }
+}
