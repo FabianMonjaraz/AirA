bc98087a90d2878f94ac901092daff91fc8e7302
FabianMonjaraz <d.fabian.monjaraz@gmail.com>
2019-10-11 17:53:10 -0500

    Leds module without serial communication

--- Arduino/mod_battery/mod_battery.ino
+++ Arduino/mod_battery/mod_battery.ino
@@ -1,15 +1,13 @@
-int measurePin = A0;
-int battLed = 4
+int measurePin = A6;
+int battLed = 7;
 float rawValue = 0;
 float voltMeasured = 0;
 float chargedBat = 4.2;
 float dischargedBat = 3.2;
-float percent = 0.0;
 int stateBat = 0;
-int aux = 0;
 
 void setup(){
-    Serial.begin(9600); 
+    //Serial.begin(9600); 
     pinMode(battLed, OUTPUT);
 }
 
@@ -23,37 +21,36 @@ void batteryOutput(int state){
             digitalWrite(battLed, LOW);
             delay(50);
             digitalWrite(battLed, HIGH);
-            delay(200);
         break;
         case 2: //Almost-dead battery
             digitalWrite(battLed, LOW);
-            delay(200);
+            delay(500);
             digitalWrite(battLed, HIGH);
-            delay(200);
-        break
+        break;
         default: //Full battery or more than 50%
             digitalWrite(battLed, HIGH);
         break;
     }
 }
 
-void loop(){
+void batteryCheck(){
     rawValue = analogRead(measurePin);
     voltMeasured = (rawValue + 0.5) * 5.0 / 1024.0;
-    percent = map(voltMeasured, dischargedBat, chargedBat, 0.0, 100.0); //Convert from voltage to percentage.
-
     batteryOutput(stateBat);
 
-    if (percent >= 51.0)
+    if (voltMeasured  >= 3.8)
         batteryOutput(0);
-    else if (percent >= 15)
+    else if (voltMeasured >= 3.4)
         batteryOutput(1);
     else
         batteryOutput(2);
-
     /*Serial.print("Raw Value: ");
     Serial.print(rawValue);
     Serial.print("; Voltage: ");
-    Serial.println(voltMeasured);*/
+    Serial.print(voltMeasured);*/
+}
+
+void loop(){
+    batteryCheck();
     delay(1000);
 }
--- Arduino/nano_leds/nano_leds.ino
+++ Arduino/nano_leds/nano_leds.ino
@@ -1,14 +1,60 @@
 int DANGER = 2;
 int WARNING = 1;
 int NORMAL = 0;
-int CO[3] = {12,11,10}; //NORMAL,WARNING,DANGER
-int PM25[3] = {9,8,7}; //NORMAL,WARNING,DANGER
-int BAT = 6;
+int CO[3] = {8,9,10}; //NORMAL,WARNING,DANGER
+int PM25[3] = {4,5,6}; //NORMAL,WARNING,DANGER
 int LimPM25[3] = {25,10}; // ug/m^3 https://www.who.int/es/news-room/fact-sheets/detail/ambient-(outdoor)-air-quality-and-health
 int LimCO[3] = {200,100}; // ppm http://www.aire.cdmx.gob.mx/default.php?opc=%27ZaBhnmI=&dc=%27Zw==
 
-float LvlCO = 73.0;
-float LvlPM25 = 15.0;
+float LvlCO = 204.0;
+float LvlPM25 = 9.0;
+
+int BAT = 7;
+int stateBat = 0;
+float dischargedBat = 3.2;
+float chargedBat = 4.2;
+float voltMeasured = 0;
+float rawValue = 0;
+int measurePin = A6;
+
+void batteryOutput(int state){
+    switch(state){
+        case 1: //Half battery or less
+            digitalWrite(BAT, LOW);
+            delay(50);
+            digitalWrite(BAT, HIGH);
+            delay(50);
+            digitalWrite(BAT, LOW);
+            delay(50);
+            digitalWrite(BAT, HIGH);
+        break;
+        case 2: //Almost-dead battery
+            digitalWrite(BAT, LOW);
+            delay(500);
+            digitalWrite(BAT, HIGH);
+        break;
+        default: //Full battery or more than 50%
+            digitalWrite(BAT, HIGH);
+        break;
+    }
+}
+
+void batteryCheck(){
+    rawValue = analogRead(measurePin);
+    voltMeasured = (rawValue + 0.5) * 5.0 / 1024.0;
+    batteryOutput(stateBat);
+
+    if (voltMeasured  >= 3.8)
+        batteryOutput(0);
+    else if (voltMeasured >= 3.4)
+        batteryOutput(1);
+    else
+        batteryOutput(2);
+    /*Serial.print("Raw Value: ");
+    Serial.print(rawValue);
+    Serial.print("; Voltage: ");
+    Serial.print(voltMeasured);*/
+}
 
 void outputLedsCO(int level){
 
@@ -57,4 +103,6 @@ void setup(){
 
 void loop(){
     measure();
+    batteryCheck();
+    delay(1000);
 }
