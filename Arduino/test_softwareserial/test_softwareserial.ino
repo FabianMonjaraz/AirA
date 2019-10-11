1e641dca20c0326015d85d8f328754eb22fb129e
FabianMonjaraz <d.fabian.monjaraz@gmail.com>
2019-10-11 14:59:15 -0500

    Antenna sending data to server

--- Arduino/test_softwareserial/test_softwareserial.ino
+++ Arduino/test_softwareserial/test_softwareserial.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 #include <string.h>
 
-SoftwareSerial mySerial(2, 3);  //RX,TX
+SoftwareSerial wifiAntenna(2, 3);  //RX,TX
 
 //  Try https://www.instructables.com/id/Arduino-Esp8266-Post-Data-to-Website/
 //  Arduino pin 2 (RX) to ESP8266 TX
@@ -9,46 +9,92 @@ SoftwareSerial mySerial(2, 3);  //RX,TX
 //  Connect GND from the Arduiono to GND on the ESP8266
 //  Pull ESP8266 CH_PD HIGH
 
-// When a command is entered in to the serial monitor on the computer 
-// the Arduino will relay it to the ESP8266
-
-char *request = "GET / HTTP/1.1\n\rHost: 192.168.43.220\n\rUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0\n\rAccept: text/html,application/xhtml+xml,application/xml;q=0.9,\n\rAccept-Language: es-MX,es;q=0.8,en-US;q=0.5,en;q=0.3\n\rAccept-Encoding: gzip, deflate\n\rConnection: keep-alive\n\rUpgrade-Insecure-Requests: 1\n\rIf-Modified-Since: Mon, 30 Sep 2019 19:02:41 GMT\n\rIf-None-Match: \"29cd-593c9e152adf9-gzip\"\n\rCache-Control: max-age=0";
-
-void sendCommand(char *cmd){
-    mySerial.println(cmd);
-    while ( mySerial.available() )   {  Serial.write( mySerial.read() );  }
-    delay(250);
+String ssid = "ADIP-DFMF";
+String pass = "bhuq0e3z";
+String host = "10.42.0.33";
+String port = "80";
+    
+void resetAntenna(){
+    wifiAntenna.println("AT+RST");
+    delay(1000);
+    if(wifiAntenna.find("OK")) 
+        Serial.println("Module Reset");
 }
- 
-void initConnection(){
-    sendCommand("AT+RST");
-    sendCommand("ATE0");
-    sendCommand("AT+GMR");
-    sendCommand("AT+CWJAP_CUR=\"ADIP-DFMF\",\"bhuq0e3z\"");
+
+
+void connectWifi(){
+    wifiAntenna.println("AT+CIPSTATUS");
+    if(wifiAntenna.find("2")||wifiAntenna.find("3")||wifiAntenna.find("4")) 
+        Serial.println("Connected to " + ssid);
+    else{
+        String cmd = "AT+CWJAP_CUR=\"" + ssid + "\",\"" + pass + "\"";
+        wifiAntenna.println(cmd);
+        delay(4000);
+        if(wifiAntenna.find("OK")) 
+            Serial.println("Connected to " + ssid);
+        else{
+            Serial.println("Error connecting to "+ ssid +". Retrying..."); 
+            connectWifi();
+        }
+    }
+
+}
+
+int setTCPConnection(){
+    wifiAntenna.println("AT+CIPSTATUS");
+    if(wifiAntenna.find("3")){ 
+        Serial.println("TCP Connection Established");
+        return 1;
+    }
+    else{
+        wifiAntenna.println("AT+CIPSTART=\"TCP\",\"" + host + "\"," + port );
+        delay(250);
+        if(wifiAntenna.find("OK")){ Serial.println("TCP Connection Established"); return 1;}
+        else{ Serial.println("Error setting TCP Connection."); return 0; }
+    }
+}
+
+int getResource(String resource){
+    String request = "GET " + resource + " HTTP/1.1\r\nHost: " + host + "\r\n\r\n";
+    int lengthRequest = request.length();
+    String cmd = "AT+CIPSEND=";
+    cmd.concat(lengthRequest);
+
+    Serial.println("Sending:");
+    Serial.println(cmd);
+    Serial.println(request);
+    wifiAntenna.println(cmd);
+    delay(100);
+
+    if(wifiAntenna.find(">")){ 
+        wifiAntenna.println(request);
+    }
     delay(1000);
 }
-void setup() 
-{
+
+void setup() {
     Serial.begin(9600);     // communication with the host computer
     //while (!Serial)   { ; }
  
     // Start the software serial for communication with the ESP8266
-    mySerial.begin(9600);  
+    wifiAntenna.begin(9600);  
  
     Serial.println("");
     Serial.println("Remember to to set Both NL & CR in the serial monitor.");
     Serial.println("Ready");
     Serial.println("");    
-    initConnection();
-    Serial.println(request);
-    Serial.println(strlen(request));
+    resetAntenna();
+    connectWifi();
+    while(!setTCPConnection());
+    
+    getResource("/medicion/7.7/3.3");
 }
  
 void loop() 
 {
     // listen for communication from the ESP8266 and then write it to the serial monitor
-    if ( mySerial.available() )   {  Serial.write( mySerial.read() );  }
+    if ( wifiAntenna.available() )   {  Serial.write( wifiAntenna.read() );  }
  
     // listen for user input and send it to the ESP8266
-    if ( Serial.available() )       {  mySerial.write( Serial.read() );  }
+    if ( Serial.available() )       {  wifiAntenna.write( Serial.read() );  }
 }
